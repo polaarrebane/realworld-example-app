6459a8b682d0b13d686497d3e4380d6b088b585a
Roni Rebane <roni.rebane@gmail.com>
2024-02-13 17:18:55 +0400

    feat: add get profile functionality

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,5 +38,11 @@ Route::namespace('Api')
             ->withoutMiddleware('protected')->group(function () {
                 Route::get('', [ArticleController::class, 'index'])->name('get');
                 Route::get('{article:slug}', [ArticleController::class, 'show'])->name('slug');
+            });
+
+        // PROFILES
+        Route::namespace('Profiles')->name('profiles.')->prefix('profiles')
+            ->withoutMiddleware('protected')->group(function () {
+                Route::get('{user:username}', [ProfileController::class, 'show'])->name('get');
             });
     });
